17df10f97330371d40d1872f4a54639bf4178cb1
Makima32 <omartorresyuste@gmail.com>
2026-05-04 12:53:46 +0100

    Comentarios en el main

--- backend/src/main/java/com/example/manga/MangaApplication.java
+++ backend/src/main/java/com/example/manga/MangaApplication.java
@@ -9,3 +9,7 @@ public class MangaApplication {
         SpringApplication.run(MangaApplication.class, args);
     }
 }
+
+
+//Se uso inteligencia artificial, gemini mas concretamente para realizar la UI le pase un figma de como lo queria , es decir el frontend, pero la estructura de carpeta y estructura general fue hecha por mi, la IA solo se encargo del diseño minimalista y responsive
+//Tambien la use para que revisara en general la estructura general y calidad del codigo pero no cambio mucho realmente
